610642cc1aba26a11a69fac7e2dea3bb2e5e2d7f
benjamin <developerkayx@gmail.com>
2018-07-18 11:17:07 +0100

    completed lodge and zone models

--- lodges/admin.py
+++ lodges/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import UserProfile, Lodge, Zone
 
 # Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(Lodge)
+admin.site.register(Zone)
--- lodges/urls.py
+++ lodges/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login, name="login"),
+    path('', views.user_login, name="login"),
     path('signup', views.signup, name="signup"),
     path('dashboard', views.dashboard, name="dashboard"),
+    path('dashboard/<str:zone_name>', views.zone, name="zone"),
 ]
