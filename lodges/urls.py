89fb138c44ccf75e474a306ef68d8248bc1c3b59
benjamin <developerkayx@gmail.com>
2018-07-19 06:08:04 +0100

    Completed Implementation with theestate template

--- lodges/urls.py
+++ lodges/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.user_login, name="login"),
-    path('signup', views.signup, name="signup"),
-    path('dashboard', views.dashboard, name="dashboard"),
-    path('dashboard/<str:zone_name>', views.zone, name="zone"),
+    path('', views.index, name="index"),
+    path('contact', views.contact, name="contact"),
+    path('news', views.news, name="news"),
+    path('elements', views.elements, name="elements"),
+    path('about', views.about, name="about"),
+    path('listings', views.listings, name="listings"),
+    path('listings_single', views.listings_single, name="listings_single"),
 ]
--- lodges/views.py
+++ lodges/views.py
@@ -5,59 +5,17 @@ from .models import UserProfile, Zone, Lodge
 from django.contrib.auth import authenticate, login
 from django.urls import reverse
 # Create your views here.
-def user_login(request):
-    if request.method == "POST":
-        username = request.POST["username"]
-        password = request.POST["password"]
-
-        user = authenticate(username=username,password=password)
-
-        if user:
-            login(request, user)
-            return HttpResponseRedirect(reverse(dashboard))
-        else:
-            return HttpResponseRedirect(reverse(login))
-
-    return render(request, 'login.html')
-
-def signup(request):
-    if request.method == "POST":
-        username = request.POST["username"]
-        email = request.POST["email"]
-        password = request.POST["password"]
-        name = request.POST["name"]
-        phone = request.POST["phone"]
-        department = request.POST["department"]
-        faculty = request.POST["faculty"]
-        level = request.POST["level"]
-        gender = request.POST["gender"]
-        study = request.POST["study"]
-        kin_name = request.POST["kin"]
-        kin_phone = request.POST["kin_phone"]
-        self_description = request.POST["description"]
-
-        user = User(username=username, email=email)
-        user.set_password(password)
-        user.save()
-        userProfile = UserProfile(user=user, full_name=name, phone=phone, department=department, faculty=faculty,level=level, gender=gender, study=study, kin_name=kin_name, kin_phone=kin_phone, self_description=self_description)
-        userProfile.save()
-        return HttpResponse("""<h1>You have successfully registered</h1><br><p>Click <a href="{% url 'login' %}">here</a> to login</p>""")
-
-    return render(request, 'signup.html')
-
-def dashboard(request):
-    user = request.user
-    if not user:
-        return HttpResponseRedirect(reverse(login))
-    context = {
-        "p" : UserProfile.objects.get(user=User.objects.get(username=user)),
-        "zones" : Zone.objects.all(),
-    }
-    return render(request, 'dashboard.html', context)
-
-def zone(request, zone_name):
-    try:
-        Zone.objects.get(name=zone_name.capitalize())
-    except Zone.DoesNotExist:
-        return Http404("Selected Zone does not exist.")
-    return HttpResponse("Correct Zone!!")
+def index(request):
+    return render(request, 'index.html')
+def contact(request):
+    return render(request, 'contact.html')
+def news(request):
+    return HttpResponse("This is The news Page.")
+def elements(request):
+    return HttpResponse("This is The elements Page.")
+def listings(request):
+    return render(request, 'listings.html')
+def listings_single(request):
+    return HttpResponse("This is The Single listings Page.")
+def about(request):
+    return render(request, 'about.html')
